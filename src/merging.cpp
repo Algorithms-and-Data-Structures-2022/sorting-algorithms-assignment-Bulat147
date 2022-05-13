15f193f178b0d2dc25e986c9dc0c0a7167f40765
ninza <ninza147258@gmail.com>
2022-05-13 14:39:46 +0300

    hmw1

--- src/merging.cpp
+++ src/merging.cpp
@@ -13,26 +13,37 @@ namespace assignment {
     int right_offset = middle + 1;
 
     // индекс текущей позиции буфера (туда будут сливаться подмассивы)
-    int buf_offset = -1 /* здесь что-то не так */;
+    int buf_offset = 0;
 
     // слияния подмассивов (пока не достигли конца одного из подмассивов)
     while (left_offset <= middle && right_offset <= stop) {
-
       // копируем элемент с наименьшим значением в буфер ...
-
+      if (arr[left_offset] < arr[right_offset]){
+        buf[buf_offset] = arr[left_offset];
+        left_offset++;
+      }
+      else{
+        buf[buf_offset] = arr[right_offset];
+        right_offset++;
+      }
       // перемещаем текущую позицию вставки в буфер
       buf_offset += 1;
     }
 
     // сливаем остатки подмассивов (останутся элементы только одного из двух подмассивов)
-    for (/* ... */; left_offset <= middle; left_offset++) {
-      buf[buf_offset] = arr[left_offset];
+    for (int left_os = left_offset; left_os <= middle; left_os++) {
+      buf[buf_offset] = arr[left_os];
       buf_offset += 1;
     }
 
     // реализуйте сливание остатков правого подмассива ...
+    for (int right_os = right_offset; right_os <= middle; right_os++) {
+      buf[buf_offset] = arr[right_os];
+      buf_offset += 1;
+    }
 
     // копируем результат слияния подмассивов из буфера в оригинальный массив ... std::copy или цикл for ...
+    std::copy(arr[0], arr[stop], buf);
   }
 
 }  // namespace assignment
